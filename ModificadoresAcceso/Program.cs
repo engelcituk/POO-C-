19c993c18fcd26eeafc1fff745b0b27a5685aa57
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-19 12:37:40 -0500

    Public y private

---
+++ ModificadoresAcceso/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ModificadoresAcceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ClasePublicaDePrueba // clase publica, se puede usar en cualquier lado
+    {
+        public int propiedadPublica { get; set; } // propiedad publica, se puede usar en cualquier lugar donde se instancie la clase
+       
+        public void MetodoPublico() // metodo publico, se puede usar en cualquier lado
+        {
+            propiedadPublica = 2;
+            metodoPrivado();// se puede llamar el metodo privado porque está dentro de esta clase
+        }
+
+        private void metodoPrivado() // este metodo privado se puede invocar solo dentro de la clase, no fuera de ella
+        {
+
+        }
+    }
+
+    public class ClasePublicaCliente
+    {
+        public void metodoPrueba()
+        {
+            var clasePublica = new ClasePublicaDePrueba(); // instancio
+            clasePublica.MetodoPublico(); // solo tengo acceso al metodo publico, no al privado  metodoPrivado()
+            // clasePublica.metodoPrivado(); // error, es privado
+        }
+    }
+}
