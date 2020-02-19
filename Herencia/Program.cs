b12e24726b54935bf07b90d62175ed1ab1fe99d3
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-19 11:04:14 -0500

    Métodos virtuales

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -9,13 +9,15 @@ namespace Herencia
            // var perro = new Perro(); // se instancia perro
             Animal perrito = new Perro(); // se instancia perro, es de tipo animal
             /*como animal es una clase de tipo abstracto no se puede instanciar*/
-            perrito.HacerRuido();
+           // perrito.HacerRuido();
 
             ProcesarAnimal(perrito);
             ProcesarAnimal(new Gato());
             static void ProcesarAnimal(Animal animal)
             {
                 animal.HacerRuido();
+                animal.comerCarne();
+
             }
 
 
@@ -46,21 +48,39 @@ namespace Herencia
         /*un metodo abstracto solo se puede crear en una clase abstracta, pero no tiene implementacion, solo se implementa en las clases derivadas 
          */
 
+        
+        public virtual void comerCarne()
+        {
+            /* Un metodo virtual es aquel que puede ser sobrescrito por una clase derivada y que tiene implementación, en c# los metodos son "NO VIRTUALES".
+          Para sobrescribir un metodo virtual,se hace igual como se hace con los metodos abstractos, se usa override
+    */
+            Console.WriteLine("comer carne yumi yumi");
+
+        }
     }
     public class Perro : Animal
     {
-        public override void HacerRuido() // HacerRuido metodo abstracto implementado
+        public override void HacerRuido() // HacerRuido metodo abstracto implementado, se sobreescribe
         {
             Console.WriteLine("woof woof");
         }
+
+        public override void comerCarne() // comerCarne metodo virtual implementado sobrescrito
+        {
+            Console.WriteLine("woof woof como carne");
+        }
+
     }
     public class Gato : Animal
     {
-        public override void HacerRuido() // HacerRuido metodo abstracto implementado
+        public override void HacerRuido() // HacerRuido metodo abstracto implementado,se sobreescribe  
         {
             Console.WriteLine("miau miau");
 
         }
+        // comerCarne metodo virtual si es implementado en esta clase, pero no es sobrescrito
     }
+
+
 
 }
